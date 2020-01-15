7169e09c428494b4d1c85ed8768f4d8c82bdc4ff
조찬영 <cyjo9603@naver.com>
2020-01-16 01:55:16 +0900

    [feat] TodoModel require 및 todolist 임시 데이터 값 생성

--- 찬영/mission8/Todo/TodoApp.js
+++ 찬영/mission8/Todo/TodoApp.js
@@ -1,5 +1,10 @@
 const util = require('./util.js');
+const TodoModel = require('./TodoModel.js');
 const TodoController = require('./TodoController.js');
 
+// 임시 데이터 값
+const todolist = {};
+
+const todoModel = new TodoModel(todolist);
 const controller = new TodoController();
 controller.runTodo();
