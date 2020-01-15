162bfc1884c6fbdbe75b38b444ad38fc66761691
조찬영 <cyjo9603@naver.com>
2020-01-16 05:41:12 +0900

    [feat] 서버에 데이터 요청 기능 구현

--- 찬영/mission8/Todo/TodoApp.js
+++ 찬영/mission8/Todo/TodoApp.js
@@ -1,15 +1,11 @@
+const request = require('../node_modules/sync-request');
 const util = require('./util.js');
 const TodoModel = require('./TodoModel.js');
 const TodoController = require('./TodoController.js');
 
-// 임시 데이터 값
-const todolist = [
-  { id: '1234', status: 'todo', contents: 'test1', tag: ['test1, test2'] },
-  { id: '1235', status: 'todo', contents: 'test2', tag: ['test1, test2'] },
-  { id: '1236', status: 'todo', contents: 'test3', tag: ['test1, test2'] },
-  { id: '1237', status: 'todo', contents: 'test4', tag: ['test1, test2'] },
-];
+const todolist = JSON.parse(request('GET', 'http://localhost:8090/get_data').getBody('utf-8'));
 
 const todoModel = new TodoModel(todolist);
 const controller = new TodoController(todoModel);
-controller.runTodo();
+//controller.runTodo();
+console.log(todolist);
