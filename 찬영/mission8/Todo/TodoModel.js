97c51f411e6b3202e82fa925676b2358cf33757d
조찬영 <cyjo9603@naver.com>
2020-01-16 02:27:23 +0900

    [feat] commandUpdate method 기능 구현

--- 찬영/mission8/Todo/TodoModel.js
+++ 찬영/mission8/Todo/TodoModel.js
@@ -13,4 +13,13 @@ TodoModel.prototype.commandAdd = function(addContents, addTag) {
   console.log(`${addContents}가 추가됐습니다. (id : ${makeId})`);
 };
 
+TodoModel.prototype.commandUpdate = function(updateId, updateStatus) {
+  this.todolist.forEach((data, index) => {
+    if (data.id === updateId) {
+      this.todolist[index].status = updateStatus;
+    }
+  });
+  console.log(`${updateStatus}으로 상태가 변경됐습니다`);
+};
+
 module.exports = TodoModel;
