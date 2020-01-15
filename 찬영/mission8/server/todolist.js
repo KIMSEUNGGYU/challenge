4d27e0e2b9ec6f0107e98120b1d1c71465b41cff
조찬영 <cyjo9603@naver.com>
2020-01-16 05:40:40 +0900

    [feat] 데이터 파일 구현

---
+++ 찬영/mission8/server/todolist.js
@@ -0,0 +1,10 @@
+todolist = {
+  data: [
+    { id: '1234', status: 'todo', contents: 'test1', tag: ['test1, test2'] },
+    { id: '1235', status: 'todo', contents: 'test2', tag: ['test1, test2'] },
+    { id: '1236', status: 'todo', contents: 'test3', tag: ['test1, test2'] },
+    { id: '1237', status: 'todo', contents: 'test4', tag: ['test1, test2'] },
+  ],
+};
+
+module.exports = todolist;
