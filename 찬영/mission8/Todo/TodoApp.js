407976a2dffaa15b2aae2803b185087107d8ffd3
조찬영 <cyjo9603@naver.com>
2020-01-16 02:37:46 +0900

    [refactor] 임시 데이터 값 추가

--- 찬영/mission8/Todo/TodoApp.js
+++ 찬영/mission8/Todo/TodoApp.js
@@ -3,7 +3,12 @@ const TodoModel = require('./TodoModel.js');
 const TodoController = require('./TodoController.js');
 
 // 임시 데이터 값
-const todolist = [];
+const todolist = [
+  { id: '1234', status: 'todo', contents: 'test1', tag: ['test1, test2'] },
+  { id: '1235', status: 'todo', contents: 'test2', tag: ['test1, test2'] },
+  { id: '1236', status: 'todo', contents: 'test3', tag: ['test1, test2'] },
+  { id: '1237', status: 'todo', contents: 'test4', tag: ['test1, test2'] },
+];
 
 const todoModel = new TodoModel(todolist);
 const controller = new TodoController(todoModel);
