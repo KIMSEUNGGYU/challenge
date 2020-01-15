ac9dab86cc73e1f2fe99acc5c47ce5d632421ea7
조찬영 <cyjo9603@naver.com>
2020-01-16 05:40:12 +0900

    [feat] 데이터 응답 기능 구현

--- 찬영/mission8/server/app.js
+++ 찬영/mission8/server/app.js
@@ -1,7 +1,10 @@
+const todolist = require('./todolist.js');
 const express = require('../node_modules/express');
 const app = express();
 const SERVER_PORT = 8090;
 
-app.get('/get_data', (req, res, next) => {});
+app.get('/get_data', (req, res, next) => {
+  return res.send(todolist);
+});
 
 app.listen(SERVER_PORT);
