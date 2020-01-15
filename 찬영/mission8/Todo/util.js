2f7ecfcff1e8e997cc63bd5069a012d424860212
조찬영 <cyjo9603@naver.com>
2020-01-16 02:07:25 +0900

    [feat] getTag 함수 기능 구현

--- 찬영/mission8/Todo/util.js
+++ 찬영/mission8/Todo/util.js
@@ -1,8 +1,13 @@
 const uuidv1 = require('../node_modules/uuid/v1');
+const EXTRACT_REG = /['" \[\]]/g;
 
 util = {
   createUniqueId: () => {
     return uuidv1();
+  },
+  getTag: string => {
+    const outputTag = string.replace(EXTRACT_REG, '').split(',');
+    return outputTag;
   },
 };
 
