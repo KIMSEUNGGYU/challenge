7f0291d25cc694e0f7e4902c86b607355efc0ace
조찬영 <cyjo9603@naver.com>
2020-01-16 02:13:09 +0900

    [feat] commandAdd method 기능 구현

--- 찬영/mission8/Todo/TodoModel.js
+++ 찬영/mission8/Todo/TodoModel.js
@@ -2,4 +2,15 @@ const TodoModel = function(todolist) {
   this.todolist = todolist;
 };
 
+TodoModel.prototype.commandAdd = function(addContents, addTag) {
+  const makeId = util.createUniqueId();
+  this.todolist.push({
+    id: makeId,
+    status: 'todo',
+    contents: addContents,
+    tag: util.getTag(addTag),
+  });
+  console.log(`${addContents}가 추가됐습니다. (id : ${makeId})`);
+};
+
 module.exports = TodoModel;
