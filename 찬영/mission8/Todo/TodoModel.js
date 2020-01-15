480079080cf266b5c0cfdf780b928c8b0efa9354
조찬영 <cyjo9603@naver.com>
2020-01-16 02:37:27 +0900

    [feat] commandDelete method 기능 구현

--- 찬영/mission8/Todo/TodoModel.js
+++ 찬영/mission8/Todo/TodoModel.js
@@ -17,9 +17,18 @@ TodoModel.prototype.commandUpdate = function(updateId, updateStatus) {
   this.todolist.forEach((data, index) => {
     if (data.id === updateId) {
       this.todolist[index].status = updateStatus;
+      console.log(`${data.contents}가 ${updateStatus}으로 상태가 변경됐습니다`);
     }
   });
-  console.log(`${updateStatus}으로 상태가 변경됐습니다`);
+};
+
+TodoModel.prototype.commandDelete = function(updateId) {
+  this.todolist.forEach((data, index) => {
+    if (data.id === updateId) {
+      this.todolist.splice(index, 1);
+      console.log(`${data.contents}가 ${data.status}목록에서 삭제됐습니다`);
+    }
+  });
 };
 
 module.exports = TodoModel;
