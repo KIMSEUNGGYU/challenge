667da8711aa9f9d8a1c25bae2f70e355b01df0da
조찬영 <cyjo9603@naver.com>
2020-01-17 17:39:57 +0900

    [feat] TodoHtmlView require

--- 찬영/mission8/Todo/TodoApp.js
+++ 찬영/mission8/Todo/TodoApp.js
@@ -2,9 +2,11 @@ const request = require('../node_modules/sync-request');
 const util = require('./util.js');
 const TodoModel = require('./TodoModel.js');
 const TodoController = require('./TodoController.js');
+const TodoHtmlView = require('./TodoHtmlView.js');
 
 const todolist = JSON.parse(request('GET', 'http://localhost:8090/get_data').getBody('utf-8'));
 
 const todoModel = new TodoModel(todolist.data);
 const controller = new TodoController(todoModel);
+new TodoHtmlView(todoModel);
 controller.runTodo();
